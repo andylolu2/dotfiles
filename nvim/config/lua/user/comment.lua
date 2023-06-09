07e25ae9f864c42c473b40d789221c14e2cc3ffb
andylolu2 <andylolu24@gmail.com>
2023-06-09 14:15:04 +0100

    Update

--- nvim/config/lua/user/comment.lua
+++ nvim/config/lua/user/comment.lua
@@ -2,11 +2,7 @@ local comment = require("Comment")
 
 comment.setup()
 
-local api = require("Comment.api")
 local opts = { noremap = true, silent = true, nowait = true }
 
-vim.keymap.set("n", "<C-_>", api.toggle.linewise.current, opts)
--- vim.keymap.set("x", "<C-_>", function()
---   vim.api.nvim_feedkeys(esc, "nx", false)
---   api.toggle.linewise(vim.fn.visualmode())
--- end, opt)
+vim.keymap.set("n", "<C-_>", "<Plug>(comment_toggle_linewise)", opts)
+vim.keymap.set("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)", opt)
