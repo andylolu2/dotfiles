b85f52f9ec7ca0577e7e46fd87df924defd71d69
andylolu2 <andylolu24@gmail.com>
2024-01-06 19:17:44 +0800

    Add script for rootless fish install

---
+++ dependencies/install_fish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ 
+# Script for installing Fish Shell on systems without root access.
+# Fish Shell will be installed in $HOME/.local
+# It's assumed that wget and following packages is installed (https://github.com/fish-shell/fish-shell#dependencies-1)
+# 1. a C++11 compiler (g++ 4.8 or later, or clang 3.3 or later)
+# 2. CMake (version 3.5 or later)
+ 
+# exit on error
+set -e
+ 
+FISH_SHELL_VERSION=3.7.0
+ 
+# create our directories
+BUILD_TEMP_DIR=/tmp/build_fish_shell
+LOCAL_DIR=$HOME/.local
+mkdir -p $LOCAL_DIR $BUILD_TEMP_DIR
+ 
+# download source files for Fish Shell
+cd $BUILD_TEMP_DIR
+wget https://github.com/fish-shell/fish-shell/releases/download/${FISH_SHELL_VERSION}/fish-${FISH_SHELL_VERSION}.tar.xz
+ 
+# extract files, configure, and compile
+tar xf fish-${FISH_SHELL_VERSION}.tar.xz
+cd fish-${FISH_SHELL_VERSION}
+
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$HOME/.local -DBUILD_SHARED_LIBS=OFF .
+make
+make install
+
